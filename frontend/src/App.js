b222d057f384f2a9560fb902149d5ed76aebfe68
fathiminha <minhasulaikar@gmail.com>
2024-06-21 07:08:06 +0530

    Initial Commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href='/'> Nature's Cart</a>
+      </header>
+      <main> List Products</main>
+    </div>
+  );
+}
+
+export default App;
