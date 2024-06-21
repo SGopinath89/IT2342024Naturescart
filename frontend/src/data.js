a2b7071431ddb6fcfcc399635ff6762021a8ef91
fathiminha <minhasulaikar@gmail.com>
2024-06-22 00:13:11 +0530

    featured produts

---
+++ frontend/src/data.js
@@ -0,0 +1,41 @@
+const data = {
+    products : [
+        {
+            name: 'Organic Carrot-300g',
+            category: 'Vegitables',
+            image: './images/v1.jpg',
+            price: 'Rs.465.00',
+            description: 'The carrot is a root vegetable often claimed to be the perfect health food.',
+        },
+        {
+            name: 'Organic Beetroot – 300g',
+            category: 'Vegitables',
+            image: './images/v2.jpg',
+            price: 'Rs.420.00',
+            description: 'Packed with essential nutrients, beetroots are a great source of fiber, folate (vitamin B9), manganese, potassium, iron, and vitamin C.',
+        },
+        {
+            name: 'Organic Pumpkin – 300g',
+            category: 'Vegitables',
+            image: './images/v3.jpg',
+            price: 'Rs.285.00',
+            description: 'Organic Pumpkin is a type of winter squash that belongs to the Cucurbitaceae family.',
+        },
+        {
+            name: 'Organic Cucumber – 300g',
+            category: 'Vegitables',
+            image: './images/v4.png',
+            price: 'Rs.225.00',
+            description: 'Organic Cucumber is high in beneficial nutrients, as well as certain plant compounds and antioxidants that may help treat and even prevent some conditions.',
+        },
+        {
+            name: 'Organic Brinjal – 300g',
+            category: 'Vegitables',
+            image: './images/v5.jpg',
+            price: 'Rs.465.00',
+            description: 'Eggplants are a nutrient-dense food, meaning they contain a good amount of vitamins, minerals and fiber in few calories.',
+        },
+    ]
+};
+
+export default data;
