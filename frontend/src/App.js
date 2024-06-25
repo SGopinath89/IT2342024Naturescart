fd8bccbab5a4623e087bbe879265cb18e4523144
fathiminha <minhasulaikar@gmail.com>
2024-06-25 05:44:36 +0530

    homescreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,36 @@
-import data from './data'
-
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
+import './index.js';
 function App() {
   return (
-    <div>
-      <header>
-        <a href='/'> Nature's Cart</a>
-      </header>
-      <main> 
-        <h1> Featured Products</h1>
-        <div className='products'>
-          {data.products.map((product) => (
-            <div className='product' key={product.slug}>
-              <a href={`/product/${product.slug}`}>
-             <img src={product.image} alt={product.name} />
-             </a>
-              <div className='product-info'>
-                <a href={`/product/${product.slug}`}>
-                <p>{product.name}</p>
-                </a>
-                <p>
-                  <strong>${product.price}</strong>
-                </p>
-                <button> Add to cart </button>
-              </div>
-            </div>
-          ))}
-        </div>
-      </main>
-    </div>
+    <BrowserRouter>
+      <div className='d-flex flex-column site-container'>
+        <header>
+           <Navbar className='navbar-custom' variant='dark'>
+            <Container>
+              <LinkContainer to = '/'>
+                <Navbar.Brand> Nature's Cart </Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
+        </header>
+        <main> 
+          <Container>
+          <Routes>
+            <Route path='/product/:slug' element={<ProductScreen />} />
+            <Route path='/' element={<HomeScreen />} />
+          </Routes> 
+          </Container>
+        </main>
+        <footer className="footer">
+          <div className='text-center'> All rights reserved </div>
+        </footer>
+      </div> 
+    </BrowserRouter>
   );
 }
 
--- frontend/src/data.js
+++ frontend/src/data.js
@@ -1,40 +1,45 @@
 const data = {
     products : [
         {
-            name: 'Organic Carrot-300g',
-            category: 'Vegitables',
-            image: './images/v1.jpg',
-            price: 'Rs.465.00',
-            description: 'The carrot is a root vegetable often claimed to be the perfect health food.',
-        },
-        {
-            name: 'Organic Beetroot – 300g',
-            category: 'Vegitables',
-            image: './images/v2.jpg',
-            price: 'Rs.420.00',
-            description: 'Packed with essential nutrients, beetroots are a great source of fiber, folate (vitamin B9), manganese, potassium, iron, and vitamin C.',
-        },
-        {
-            name: 'Organic Pumpkin – 300g',
-            category: 'Vegitables',
-            image: './images/v3.jpg',
-            price: 'Rs.285.00',
-            description: 'Organic Pumpkin is a type of winter squash that belongs to the Cucurbitaceae family.',
-        },
-        {
             name: 'Organic Cucumber – 300g',
+            slug: 'cucumber',
             category: 'Vegitables',
             image: './images/v4.png',
             price: 'Rs.225.00',
-            description: 'Organic Cucumber is high in beneficial nutrients, as well as certain plant compounds and antioxidants that may help treat and even prevent some conditions.',
+            description: 'Eggplants are a nutrient-dense food, meaning they contain a good amount of vitamins, minerals and fiber in few calories.',
         },
         {
+            name: 'Keepwell Organic Chickpea Flour – 400g',
+            slug: 'Chickpea-Flour',
+            category: 'Grains',
+            image: './images/g2.jpg',
+            price: 'Rs.800.00',
+            description: 'Keepwell Organic Chickpea Flour is also known as Besan Flour. Naturally dense flour with good binding tendencies.',
+        },
+        {
+            name: 'Organic Pineapple – 500g',
+            slug: 'pineapple',
+            category: 'Fruits',
+            image: './images/f1.jpg',
+            price: 'Rs.850.00',
+            description: 'Pineapple and its compounds have been linked to many health benefits, including aiding digestion, boosting immunity and speeding up recovery from surgery, among others.',
+        },
+        {
+            name: 'Organic Dairy Milk – Low Fat – 1L',
+            slug: 'milk',
+            category: 'Diary',
+            image: './images/d1.jpg',
+            price: 'Rs.900.00',
+            description: 'Living Planet Organic Dairy Milk.',
+        }, 
+        /* {
             name: 'Organic Brinjal – 300g',
+            slug: 'brinjal',
             category: 'Vegitables',
             image: './images/v5.jpg',
             price: 'Rs.465.00',
-            description: 'Eggplants are a nutrient-dense food, meaning they contain a good amount of vitamins, minerals and fiber in few calories.',
-        },
+            description: '',
+        },  */
     ]
 };
 
