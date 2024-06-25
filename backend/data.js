b60e3bdd7e2c91ac9d6259c69eb0185349e3ddb2
fathiminha <minhasulaikar@gmail.com>
2024-06-25 05:41:26 +0530

    backend

---
+++ backend/data.js
@@ -0,0 +1,46 @@
+const data = {
+    products : [
+        {
+            name: 'Organic Cucumber – 300g',
+            slug: 'cucumber',
+            category: 'Vegitables',
+            image: './images/v4.png',
+            price: 'Rs.225.00',
+            description: 'Eggplants are a nutrient-dense food, meaning they contain a good amount of vitamins, minerals and fiber in few calories.',
+        },
+        {
+            name: 'Keepwell Organic Chickpea Flour – 400g',
+            slug: 'Chickpea-Flour',
+            category: 'Grains',
+            image: './images/g2.jpg',
+            price: 'Rs.800.00',
+            description: 'Keepwell Organic Chickpea Flour is also known as Besan Flour. Naturally dense flour with good binding tendencies.',
+        },
+        {
+            name: 'Organic Pineapple – 500g',
+            slug: 'pineapple',
+            category: 'Fruits',
+            image: './images/f1.jpg',
+            price: 'Rs.850.00',
+            description: 'Pineapple and its compounds have been linked to many health benefits, including aiding digestion, boosting immunity and speeding up recovery from surgery, among others.',
+        },
+        {
+            name: 'Organic Dairy Milk – Low Fat – 1L',
+            slug: 'milk',
+            category: 'Diary',
+            image: './images/d1.jpg',
+            price: 'Rs.900.00',
+            description: 'Living Planet Organic Dairy Milk.',
+        },
+        /* {
+            name: 'Organic Brinjal – 300g',
+            slug: 'brinjal',
+            category: 'Vegitables',
+            image: './images/v5.jpg',
+            price: 'Rs.465.00',
+            description: '',
+        }, */
+    ]
+};
+
+export default data;
