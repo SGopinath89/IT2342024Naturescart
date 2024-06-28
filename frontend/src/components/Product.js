be7652c9e1b0fd2eeb9cec400ab5383d4c2370df
fathiminha <minhasulaikar@gmail.com>
2024-06-28 09:35:13 +0530

    Add to cart screen

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -2,9 +2,36 @@ import { Link } from 'react-router-dom';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import Rating from './Rating';
+import axios from 'axios';
+import { useContext, useState } from 'react';
+import { Store } from '../Store';
+
+axios.defaults.baseURL = 'http://localhost:3000';
 
 function Product(props) {
     const {product} = props;
+    const [isDisableBtn, setIsDisableBtn] = useState(false);
+
+    const { state, dispatch: ctxDispatch } = useContext(Store);
+    const {
+        cart: { cartItems },
+    } = state;
+
+    const addToCartHandler = async (item) =>{
+        const existItem = cartItems.find((x) => x._id === product._id);
+        const quantity = existItem ? existItem.quantity + 1 : 1;
+
+        const { data } = await axios.get(`/api/products/${item._id}`);
+        if(data.countInStock < quantity) {
+            window.alert('Sorry. Product is out of stock');
+            setIsDisableBtn(true);
+            return;
+        }
+        ctxDispatch({
+            type: 'CART_ADD_ITEM',
+            payload: { ...item, quantity },
+        }); 
+    }
     return(
         <Card className="product-card">
             <Link to={`/product/${product.slug}`}>
@@ -16,8 +43,15 @@ function Product(props) {
                 </Link>
                 <Rating rating={product.rating} numReviews={product.numReviews} />
                 <Card.Text>{product.price}</Card.Text>
-                <Button>Add to cart</Button>
+                
             </Card.Body>
+            {isDisableBtn ? (
+                <Button variant='light' disabled>
+                    Out of Stock
+                </Button>
+            ) : (
+                <Button onClick={() => addToCartHandler(product)}>Add to cart</Button>
+            )}
         </Card>
     )
 }
